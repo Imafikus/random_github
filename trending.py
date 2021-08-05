4ca95deb2646bfd0569fef881d6aa37a2834a8dc
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-08-05 22:22:25 +0200

    First version of trending extraction

--- trending.py
+++ trending.py
@@ -7,10 +7,20 @@ def extract_trending_repos():
     
     soup = BeautifulSoup(raw_html, 'html.parser')
     
-    # for article in soup.find_all('article'):
-    #     for link in article.get('a'):
-    #         print(link)
+    for article in soup.find_all('article', class_='Box-row'):
+        repo = article.find_next('h1', class_='h3 lh-condensed')
+        repo_link = repo.find_next('a').get('href')
+        
+        language_obj = article.find_next('span',itemprop="programmingLanguage")
+        if language_obj == None:
+            continue
+        
+        language = language_obj.text
+        
+        print(repo_link)
+        print(language)
+        print()
     
-    for repo in soup.find_all("h1", class_="h3 lh-condensed"):
-        data = repo.find_next('a')
-        print(data.get('href'))
+    # for repo in soup.find_all("h1", class_="h3 lh-condensed"):
+    #     data = repo.find_next('a')
+    #     print(data.get('href'))
