87c050ce8ac4a362fdb4c8a6db5c6cef08af1148
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-08-12 19:04:46 +0200

    Rename api.py to github_api.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import data_extractor
 import comment_extractor
-import api
-
+import github_api
 if __name__ == "__main__":
     repos = data_extractor.get_repos_with_supported_languages()
     test = repos[0]
@@ -11,7 +10,7 @@ if __name__ == "__main__":
     language_specific_files = data_extractor.extract_all_language_files(files, test.language)
     for f in language_specific_files:
         print(f.download_url)
-        file_content = api.get_raw_data(f.download_url)
+        file_content = github_api.get_raw_data(f.download_url)
         print(f'file_comments: {comment_extractor.extract_python_comments(file_content)}')
         print()
         
