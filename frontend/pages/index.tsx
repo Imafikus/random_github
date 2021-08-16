8995c1ad0871ab12d0b86a00b0cdd7f5f724516f
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-08-16 15:25:45 +0200

    Add dummy button action

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,8 +2,12 @@ import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 
 export default function Home() {
+  const getComment = () => {
+    console.log('getComment');
+  }
+  
   return (
-    <div className='flex flex-wrap w-screen h-screen' >
+    <div className='flex flex-wrap w-screen h-screen bg-gray-900 text-white' >
       <Head>
         <title>Random github</title>
         <link rel='icon' href='/favicon.ico' />
@@ -11,8 +15,11 @@ export default function Home() {
 
       <main className='m-auto'>
         <div className='text-center'>
-          <p className='text-3xl'>Random github comments every day</p>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+          <p className='text-3xl p-2 my-2'>Random github comments every day</p>
+          <button 
+            className='w-full p-2 bg-blue-500 hover:bg-blue-700 text-white text-2xl font-bold py-2 px-4 rounded-full'
+            onClick={getComment}
+          >
             Get random comment
           </button>
         </div>
