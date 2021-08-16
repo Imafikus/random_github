b626d3fbcb5fdb31db96f30e982e439e50263205
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-08-16 14:55:12 +0200

    Add basic layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,15 +3,18 @@ import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap w-screen" >
+    <div className='flex flex-wrap w-screen h-screen' >
       <Head>
         <title>Random github</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className='m-auto'>
-        <div>
+        <div className='text-center'>
           <p className='text-3xl'>Random github comments every day</p>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+            Get random comment
+          </button>
         </div>
       </main>
 
