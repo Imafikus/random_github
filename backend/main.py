9b42f399847d3aed8aa2bce46ea634086027ac9a
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-10-08 19:11:12 +0200

    Don't use CORS

--- backend/main.py
+++ backend/main.py
@@ -3,19 +3,19 @@ from fastapi.middleware.cors import CORSMiddleware
 import datastore_db as db
 import random
 
-origins = [
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+# ]
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get('/comment')
