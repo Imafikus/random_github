27abad099ad05cd6d53c082c98b280e0ed31beff
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-10-09 23:00:01 +0200

    WIP

--- backend/comment_extractor.py
+++ backend/comment_extractor.py
@@ -1,5 +1,5 @@
 from models import ChoosenComment
-from typing import List
+from typing import List, Optional
 import re
 import data_extractor
 import github_api
@@ -15,7 +15,8 @@ load_dotenv()
 logging.basicConfig(format='[%(levelname)s]: %(asctime)s @ %(filename)s/%(funcName)s:%(lineno)d - %(message)s ', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 
-MAX_COMMENT_NUMBER = int(os.environ['MAX_COMMENT_NUMBER'])
+MAX_COMMENT_NUMBER_PER_REPO = int(os.environ['MAX_COMMENT_NUMBER_PER_REPO'])
+MAX_COMMENT_NUMBER_GLOBAL = int(os.environ['MAX_COMMENT_NUMBER_GLOBAL'])
 CURRENT_ENV = os.environ['ENV']
 
 test_comments = [
@@ -41,7 +42,7 @@ test_comments = [
     ),
 ]
 
-def extract_python_comments(file_content, file_url) -> List[ChoosenComment]:
+def extract_single_random_python_comment(file_content, file_url) -> Optional[ChoosenComment]:
     comments = re.findall(r'#.*', file_content)
     cleaned_comments = []
     
@@ -54,27 +55,42 @@ def extract_python_comments(file_content, file_url) -> List[ChoosenComment]:
             )
             cleaned_comments.append(comment)
     
-    return cleaned_comments
+    return random.choice(cleaned_comments)
     
 def get_all_choosen_comments() -> List[ChoosenComment]:
     
-    if CURRENT_ENV == 'test':
-        logging.info('Returning test_comments...')
-        return test_comments
+    # if CURRENT_ENV == 'test':
+    #     logging.info('Returning test_comments...')
+    #     return test_comments
     
     chosen_comments = []
     
     repos = data_extractor.get_repos_with_supported_languages()
     for repo in repos:
+        logging.info(f'Currently extracting from repo: {repo.name}')
+        
+        repo_comments = []
         files = data_extractor.get_all_files(repo)
         language_specific_files = data_extractor.extract_all_language_files(files, repo.language)
+        logging.info(f'Number of language specific files found: {len(language_specific_files)}')
+        
+        
         for f in language_specific_files:
             
             file_content = github_api.get_raw_data(f.download_url)
-            chosen_comments += extract_python_comments(file_content, f.html_url)
+            repo_comments.append(extract_single_random_python_comment(file_content, f.html_url))
         
-        if len(chosen_comments) >= MAX_COMMENT_NUMBER:
+            if len(repo_comments) >= MAX_COMMENT_NUMBER_PER_REPO:
+                logging.info(f'Max comment number reached for repo: {repo.name}')
+                break
+        
+        chosen_comments += repo_comments
+        logging.info(f'Current comment number: {len(chosen_comments)}')
+        
+        if len(chosen_comments) >= MAX_COMMENT_NUMBER_GLOBAL:
+            logging.info(f'Max comment number reached globally, wrapping up...')
             break
+    
     return chosen_comments
     
 def main(message, context):
