ce3dfe71f13e28599f5fd02ecc81fa573645750e
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-10-09 16:10:35 +0200

    Add loading bar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,17 +2,19 @@ import Head from 'next/head'
 import { useState } from 'react';
 import 'tailwindcss/tailwind.css'
 import * as api from '../api';
+import ClipLoader from "react-spinners/ClipLoader";
 
-export default function Home() {
-  
+export default function Home() {  
   const [currentlyDisplayedCommentContent, setCurrentlyDisplayedCommentContent] = useState('Random github comments every day');
   const [currentlyDisplayedCommentSource, setCurrentlyDisplayedCommentSource] = useState('');
-  
+  const [isLoading, setIsLoading] = useState(false);
   
   const getComment = async () => {
+    setIsLoading(true);
     const comment = await api.getComment();
     setCurrentlyDisplayedCommentContent(comment.content);
     setCurrentlyDisplayedCommentSource(comment.url);
+    setIsLoading(false);
   }
   
   const displayLinkIfNeeded = () => {
@@ -37,6 +39,9 @@ export default function Home() {
       <main className='m-auto w-2/3 sm:w-full'>
         <div className='text-left sm:text-center p-2 my-2'>
           <p className='text-2xl md:text-3xl'>{currentlyDisplayedCommentContent}</p>
+          <div className='mt-5 text-center'>
+            <ClipLoader loading={isLoading} color='white' size={45}/>
+          </div>
           <div className='mt-5'>
             <button 
               className=' bg-blue-500 hover:bg-blue-700 text-white text-xl md:text-2xl font-bold py-4 px-6 rounded-full mb-5'
