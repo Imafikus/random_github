a12d733b6cefdbc5bfd8a8897d8e99c714a71f9a
Aleksa Tesic <aleksatesicteske@gmail.com>
2021-10-11 20:23:37 +0200

    Remove unused variables from FE terraform

--- frontend/variables.tf
+++ frontend/variables.tf
@@ -1,13 +1,3 @@
-variable "github_username" {
-    type        = string
-    description = "Username used when consuming GitHub API"
-    default = "imafikus"
-}
-variable "github_access_token" {
-    type        = string
-    description = "Token used when consuming GitHub API"
-    sensitive   = true
-}
 variable "commit_sha" {
     type        = string
     description = "Commit sha for the currently running job"
